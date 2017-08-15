97c4791887da9aed75e0354c833a37e4b0d33190
Wilson Hobbs <wilsonhobbs1@gmail.com>
2017-08-15 15:29:54 -0400

    start work for 1.0

--- src/arithmetic/subtract.ts
+++ src/arithmetic/subtract.ts
@@ -1,52 +1,42 @@
+import { checkStrings } from '../util/check-strings';
 /**
  * Subtract the first number from the second
  * @export
- * @param {number} a 
- * @param {number} b 
+ * @param {number} a The from which to subtract
+ * @param {number} b The number to subtract
+ * @param {boolean} [strict] Throw an error if the parameters are not numbers or valid number strings
  * @returns {number} 
+ * 
+ * @usage
+ * 
  */
-export function subtract(a: number | string, b: number | string): number {
-    checkStrings(a, b);
+export function subtract(a: number | string, b: number | string, strict?: boolean): number {
+    if (strict) checkStrings(a, b);
     return +a - +b;
 }
 
 /**
  * Subtract the smaller parameter from the larger, always resulting in a 0 or positive number.
  * @export
- * @param {(number | string)} a 
- * @param {(number | string)} b 
+ * @param {(number | string)} a number from which to subtract
+ * @param {(number | string)} b number from which to subtract
+ * @param {boolean} [strict] Throw an error if the parameters are not numbers or valid number strings
  * @returns {number} 
  */
-export function subtractSmallerFromLarger(a: number | string, b: number | string): number {
-    checkStrings(a, b);
-    try {
-        return a > b ? +b - +a : +a - +b;
-    } catch (e) {
-        throw new Error(e);
-    }
+export function subtractSmallerFromLarger(a: number | string, b: number | string, strict?: boolean): number {
+    if (strict) checkStrings(a, b);
+    return +a > +b ? +b - +a : +a - +b;
 }
 
 /**
  * Subtract the larger parameter from the smaller, always resulting in a 0 or negative number.
  * @export
- * @param {(number | string)} a 
- * @param {(number | string)} b 
+ * @param {(number | string)} a number from which to subtract
+ * @param {(number | string)} b number from which to subtract
+ * @param {boolean} [strict] Throw an error if the parameters are not numbers or valid number strings
  * @returns {number} 
  */
-export function subtractLargerFromSmaller(a: number | string, b: number | string): number {
-    checkStrings(a, b);
-    try {
-        return a > b ? +a - +b : +b - +a;
-    } catch (e) {
-        throw new Error(e);
-    }
-}
-
-/**
- * Checks if your string is a valid number, and warns you if it is not.
- * @param {(number | string)} a 
- * @param {(number | string)} b 
- */
-function checkStrings(a: number | string, b: number | string): void {
-    if ((+a === 0 && a !== '0') || (+b === 0 && b !== '0')) console.warn('One of your parameters is a non-number string. These are treated as 0 in JavaScript, and may cause trouble in your code.')
+export function subtractLargerFromSmaller(a: number | string, b: number | string, strict?: boolean): number {
+    if (strict) checkStrings(a, b);
+    return +a > +b ? +a - +b : +b - +a;
 }
